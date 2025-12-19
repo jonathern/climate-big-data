371f2bb121a14e2362bbe321199b1d9b554d8f9d
Jonathan Kateega <33865040+ruthlesscarrot@users.noreply.github.com>
2025-12-19 07:34:59 +0300

    cleaner weather stations map visualization

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="africa-weather-ml",
-    version="1.0.0",
+    version="1.1.0",
     description="Extreme Weather Event Prediction for Africa",
     packages=find_packages(where="src"),
     package_dir={"": "src"},
